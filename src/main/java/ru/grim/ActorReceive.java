c501546280a145c32364215ad5327b29ea4f7a3b
grim <dima.ggrimbergg@gmail.com>
2020-12-18 12:38:03 +0300

    Scripted auto-commit on change (2020-12-18 12:38:03) by gitwatch.sh

--- src/main/java/ru/grim/ActorReceive.java
+++ src/main/java/ru/grim/ActorReceive.java
@@ -1,5 +1,16 @@
 package ru.grim;
 
 
+import akka.actor.AbstractActor;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ActorReceive extends AbstractActor {
+    private final Map<String, Integer> container = new HashMap<>();
+
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
